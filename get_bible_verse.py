10597c90961b74ce517b4ffb0a420466230e7009
Andy Berglund <andy.berglund@windsorcircle.com>
2015-08-18 14:11:12 -0400

    Commented out what isn't currently being used.

--- get_bible_verse.py
+++ get_bible_verse.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python
 
-import os
+#import datetime
 import json
-import datetime
+#import os
 
 import requests
-# import suds
 
 class BibleVerseGrabber(object):
+    """
+    Access the ESV Bible API to get verses/chapters of the Bible.
+    Functionality to pull specific Book/Chapter/Verse forthcoming.
+    """
     def __init__(self):
-        self.translation = os.getenv('BIBLE_TRANSLATION', 'ESV')
+        pass
+        # self.translation = os.getenv('BIBLE_TRANSLATION', 'ESV')
         
-        verse_date_string = os.environ.get('VERSE_DATE')
-        if not verse_date_string:
-            print "VERSE_DATE Environment Variable not set."
-            self.__del__()
+        # verse_date_string = os.environ.get('VERSE_DATE')
+        # if not verse_date_string:
+        #     print "VERSE_DATE Environment Variable not set."
+        #     self.__del__()
 
-        verse_date = datetime.datetime.strptime(verse_date_string, '%Y-%m-%d')
-        verse_date = verse_date.date()
-        today = datetime.datetime.now().date()
-        time_delta = today - verse_date
+        # verse_date = datetime.datetime.strptime(verse_date_string, '%Y-%m-%d')
+        # verse_date = verse_date.date()
+        # today = datetime.datetime.now().date()
+        # time_delta = today - verse_date
 
-        if time_delta.days < 1:
-            self.__del__()
+        # if time_delta.days < 1:
+        #     self.__del__()
 
 
-        today_string = datetime.datetime.strftime(today, '%Y-%m-%d')
-        os.environ['VERSE_DATE'] = today_string
+        # today_string = datetime.datetime.strftime(today, '%Y-%m-%d')
+        # os.environ['VERSE_DATE'] = today_string
 
     def __del__(self):
         pass
         
     def _make_request(self):
-
         if self.translation == 'ESV':
             api_url = "http://www.esvapi.org/v2/rest/verse?key=IP&include-footnotes=false&output-format=plain-text"
         response = requests.get(api_url)
@@ -40,5 +43,4 @@ class BibleVerseGrabber(object):
 
 if __name__ == '__main__':
     verse_grab = BibleVerseGrabber()
-
     verse_grab._make_request()
